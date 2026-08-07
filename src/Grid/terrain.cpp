64c9a1adeea5e50377bb20ce833ab01b83b1d5df
Alex/Eris Norton <Alexandersnorton23@gmail.com>
2026-08-07 20:18:22 +0100

    removed unnecessary this->

--- src/Grid/terrain.cpp
+++ src/Grid/terrain.cpp
@@ -9,26 +9,26 @@ Terrain::Terrain(float groundTraversability, float coastalTraversability, float 
 }
 
 float Terrain::get_grounded_traversability(){
-    return this->traversability[0];
+    return traversability[0];
 }
 float Terrain::get_coastal_traversability(){
-    return this->traversability[1];
+    return traversability[1];
 }
 float Terrain::get_naval_traversability(){
-    return this->traversability[2];
+    return traversability[2];
 }
 void Terrain::set_grounded_traversability(float newValue){
-    this->traversability[0] = newValue;
+    traversability[0] = newValue;
 }
 void Terrain::set_coastal_traversability(float newValue){
-    this->traversability[1] = newValue;
+    traversability[1] = newValue;
 }
 void Terrain::set_naval_traversability(float newValue){
-    this->traversability[2] = newValue;
+    traversability[2] = newValue;
 }
 
 void Terrain::describe(){
-    std::cout << "grounded: " << this->get_grounded_traversability() << std::endl;
-    std::cout << "coastal: " << this->get_coastal_traversability() << std::endl;
-    std::cout << "naval: " << this->get_naval_traversability() << std::endl;
+    std::cout << "grounded: " << get_grounded_traversability() << std::endl;
+    std::cout << "coastal: " << get_coastal_traversability() << std::endl;
+    std::cout << "naval: " << get_naval_traversability() << std::endl;
 }
